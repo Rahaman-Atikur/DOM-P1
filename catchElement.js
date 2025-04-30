32b3f2543b614f58c3257af7715245be952101bf
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-30 22:04:43 +0100

    some event handler

--- catchElement.js
+++ catchElement.js
@@ -1,2 +1,5 @@
-document.getElementById('title-one').addEventListener()
-console.log(tagName);
+document.getElementById('login-btn')
+    .addEventListener('click', handleClick);
+function handleClick() {
+    document.getElementById('title-one').style.color = 'blue';
+}

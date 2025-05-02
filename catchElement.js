23045aa72cb8385f30821c7d214eb9047da23d49
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-03 00:13:09 +0100

    Not working but no code issues

--- catchElement.js
+++ catchElement.js
@@ -3,7 +3,21 @@ document.getElementById('add-product-btn')
         const productNameInputField = document.getElementById('product-name-input');
         const productLinkInputField = document.getElementById('product-link-id');
         const productPriceInputField = document.getElementById('product-price-id');
-        console.log(productNameInputField.value);
-        
+
+        const mainContainer = document.getElementById('output-div');
+
+        const writtenInBoxName = productNameInputField.value;
+        const writtenInBoxLink = productLinkInputField.value;
+        const writtenInBoxPrice = productPriceInputField.value;
+
+
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <p>${writtenInBoxName}</p>
+        <p>${writtenInBoxLink}</p>
+        <p>${writtenInBoxPrice}</p>
+        `;
+
+        mainContainer.appendChild(div);
 
     })
